2667d1d0545f60a356d587c20c252e59c203ee6c
kungfukitty <46254915+kungfukitty@users.noreply.github.com>
2025-10-04 19:28:24 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
-import { defineConfig } from 'vite'\nimport react from '@vitejs/plugin-react'\nexport default defineConfig({ plugins:[react()], server:{port:5173}, build:{outDir:'dist', sourcemap:false} })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: { port: 5173 },
+  build: { outDir: 'dist', sourcemap: false }
+})
